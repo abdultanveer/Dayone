5a47dc9a97f39ba55ab43ea559c2e7cb3871edbd
abdul <abdul.tanveer@gmail.com>
2019-01-17 11:49:17 +0530

    text to speech

--- app/src/main/java/com/example/admin/dayone/MainActivity.java
+++ app/src/main/java/com/example/admin/dayone/MainActivity.java
@@ -3,18 +3,27 @@ package com.example.admin.dayone;
 import android.content.ContentResolver;
 import android.database.Cursor;
 import android.net.Uri;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
     public static String TAG = MainActivity.class.getSimpleName();
+
+    TextToSpeech textToSpeech;
+    String sms;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textToSpeech = new TextToSpeech(MainActivity.this, this);
         Log.i(TAG, "onCreate");
         /*ProgressBar progressBar = findViewById(R.id.progressBar);
         DownloadTask downloadTask = new DownloadTask(progressBar);
@@ -24,13 +33,20 @@ public class MainActivity extends AppCompatActivity {
         //Step1: connect to the content provider
         ContentResolver contentResolver = getContentResolver();
         //step 2: query the content provider
-        Cursor cursor = contentResolver.query(tableName,null,null,null,null);
+        Cursor cursor = contentResolver.query(tableName, null, null, null, null);
         cursor.moveToFirst();
         int bodyColumnIndex = cursor.getColumnIndexOrThrow("body");
-        String sms = cursor.getString(bodyColumnIndex);
+         sms = cursor.getString(bodyColumnIndex);
         //step3: get the data from cursor and show in the textview
         TextView smsTextView = findViewById(R.id.textViewSms);
         smsTextView.setText(sms);
+
+        smsTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textToSpeech.speak(sms,TextToSpeech.QUEUE_FLUSH,null);
+            }
+        });
 
 
     }
@@ -39,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Log.d(TAG, "onStart");
-       int c = add(10,20);
+        int c = add(10, 20);
     }
 
     @Override
@@ -50,8 +66,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private int add(int a, int b){
-        return a+b;
+    private int add(int a, int b) {
+        return a + b;
     }
 
     @Override
@@ -65,4 +81,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
     }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+
+            int result = textToSpeech.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!");
+        }
+
+    }
+
 }
+
